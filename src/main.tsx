82f925f61c8516d20a044e7637e808d7f64fc728
Danylo Fomin <fomindanylo@gmail.com>
2025-05-06 22:24:11 +0300

    Added router, helmet and first boards list page

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import App from './App.tsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './store/store.ts'
+import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
